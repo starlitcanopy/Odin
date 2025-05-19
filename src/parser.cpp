ef03c38b341bb533f2c80ab13ee08a59f55251b5
prescientmoon <git@moonythm.dev>
2025-05-19 18:58:34 +0200

    Fix string indexing

--- src/parser.cpp
+++ src/parser.cpp
@@ -5749,7 +5749,7 @@ bool collect_odin_files_recursive(String path, Array<FileInfo> *files) {
 
 	for (FileInfo fi : list) {
 		String ext = path_extension(fi.name);
-		if (fi.is_dir && fi.name.length > 0 && fi.name.cstring[0] == '_') {
+		if (fi.is_dir && fi.name.length > 0 && fi.name[0] == '_') {
 			ok = ok && collect_odin_files_recursive(fi.fullpath, files);
 		} else if (ext == FILE_EXT) {
 			array_add(files, fi);
